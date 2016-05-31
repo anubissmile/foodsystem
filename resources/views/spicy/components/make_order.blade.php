42d2a8749708cf05dd6bb3edfee8c5095d9326c1
Anubisunpluged <wisarut.khm@spulive.net>
2016-05-31 12:41:31 +0700

    SAVE
    SAVE

---
+++ resources/views/spicy/components/make_order.blade.php
@@ -0,0 +1,66 @@
+<section id="mu-make-order">
+	    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="mu-make-order-area">
+            <div class="mu-title">
+              <span class="mu-subtitle">Make Order</span>
+              <h2>สั่งอาหาร</h2>
+              <i class="fa fa-spoon"></i>              
+              <span class="mu-title-bar"></span>
+            </div>
+            <div class="mu-make-order-content">
+              <div class="row" id=".noodle" data-type="select">
+                <h3>เลือกเส้น</h3>
+                <div class="noodle mu-readmore-btn" data-price="0">เส้นเล็ก</div>
+                <div class="noodle mu-readmore-btn" data-price="0">เส้นใหญ่</div>
+                <div class="noodle mu-readmore-btn" data-price="0">หมี่ขาว</div>
+                <div class="noodle mu-readmore-btn" data-price="0">บะหมี่</div>
+                <div class="noodle mu-readmore-btn" data-price="0">วุ้นเส้น</div>
+                <div class="noodle mu-readmore-btn" data-price="0">มาม่า</div>
+              </div>
+              <div class="row" id=".soup" data-type="select">
+                <h3>เลือกน้ำซุป</h3>
+                <div class="soup mu-readmore-btn" data-price="30">ต้มยำน้ำข้น 30฿</div>
+                <div class="soup mu-readmore-btn" data-price="30">ต้มยำน้ำใส 30฿</div>
+                <div class="soup mu-readmore-btn" data-price="20">น้ำใส 20฿</div>
+                <div class="soup mu-readmore-btn" data-price="20">เย็นตาโฟ 20฿</div>
+                <div class="soup mu-readmore-btn" data-price="30">ต้มยำแห้ง 30฿</div>
+                <div class="soup mu-readmore-btn" data-price="20">แห้ง 20฿</div>
+              </div>
+              <div class="row" id=".topping" data-type="select">
+                <h3>เลือกท็อปปิง</h3>
+                <div class="topping mu-readmore-btn" data-price="0">ลูกชิ้นไก่</div>
+                <div class="topping mu-readmore-btn" data-price="0">ลูกชิ้นปลา</div>
+                <div class="topping mu-readmore-btn" data-price="0">เกี๊ยว</div>
+              </div>
+              <div class="row" id=".other" data-type="choice">
+                <h3>คุณสมบัติอื่นๆ</h3>
+                <div class="other mu-readmore-btn" data-price="0">ไม่ใส่ผัก</div>
+                <div class="other mu-readmore-btn" data-price="5">เพิ่มไข่ +5฿</div>
+              </div>
+              <div class="row" id=".extra" data-type="select">
+                <h3>พิเศษ</h3>
+                <div class="extra mu-readmore-btn" data-price="5">พิเศษ +5฿</div>
+                <div class="extra mu-readmore-btn" data-price="30">ยกทัพ +30฿</div>
+              </div>
+              <div class="row" id=".amount" data-type="hit">
+                <h3>จำนวน</h3>
+                <button id="plus" class="amount mu-readmore-btn" data-price="0">+</button>
+                <button id="minus" class="amount mu-readmore-btn" data-price="0">-</button>
+                <div id="amount" class="amount mu-readmore-btn-active">1 ชาม</div>
+              </div>
+              <div class="row" id=".price" data-type="none">
+                <h3>ราคาต่อชาม</h3>
+                <div id="price" class="amount mu-readmore-btn-active">1 บาท</div>
+              </div>
+              <div class="row" id=".sum" data-type="none">
+                <h3>ราคาทั้งสิ้น</h3>
+                <div id="sum" class="amount mu-readmore-btn-active">1 บาท</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+</section>
