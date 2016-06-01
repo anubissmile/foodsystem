0c302c1a8690c7e844b584deea297633e11ab339
Anubisunpluged <wisarut.khm@spulive.net>
2016-06-01 11:20:42 +0700

    SAVE PATCH
    - Save patch.

--- resources/views/spicy/components/make_order.blade.php
+++ resources/views/spicy/components/make_order.blade.php
@@ -3,12 +3,12 @@
       <div class="row">
         <div class="col-md-12">
           <div class="mu-make-order-area">
-            <div class="mu-title">
+            <!-- <div class="mu-title">
               <span class="mu-subtitle">Make Order</span>
               <h2>สั่งอาหาร</h2>
               <i class="fa fa-spoon"></i>              
               <span class="mu-title-bar"></span>
-            </div>
+            </div> -->
             <div class="mu-make-order-content">
               <div class="row" id=".noodle" data-mustselect="true" data-type="select">
                 <h3>เลือกเส้น</h3>
@@ -33,37 +33,40 @@
                 <div class="topping mu-readmore-btn" data-price="0">ลูกชิ้นไก่</div>
                 <div class="topping mu-readmore-btn" data-price="0">ลูกชิ้นปลา</div>
                 <div class="topping mu-readmore-btn" data-price="0">เกี๊ยว</div>
+                <div class="topping mu-readmore-btn-disable" data-price="0"></div>
+                <div class="topping mu-readmore-btn-disable" data-price="0"></div>
+                <div class="topping mu-readmore-btn-disable" data-price="0"></div>
               </div>
               <div class="row" id=".other" data-mustselect="false" data-type="choice">
                 <h3>คุณสมบัติอื่นๆ (เลือกได้หลายรายการ)</h3>
                 <div class="other mu-readmore-btn" data-price="0">ไม่ใส่ผัก</div>
                 <div class="other mu-readmore-btn" data-price="5">เพิ่มไข่ (5฿)</div>
+                <div class="other mu-readmore-btn-disable" data-price="0"></div>
+                <div class="other mu-readmore-btn-disable" data-price="0"></div>
+                <div class="other mu-readmore-btn-disable" data-price="0"></div>
+                <div class="other mu-readmore-btn-disable" data-price="0"></div>
               </div>
               <div class="row" id=".extra" data-mustselect="false" data-type="select">
                 <h3>พิเศษ</h3>
                 <div class="extra mu-readmore-btn" data-price="5">พิเศษ (5฿)</div>
                 <div class="extra mu-readmore-btn" data-price="30">ยกทัพ (30฿)</div>
+                <div class="extra mu-readmore-btn-disable" data-price="0"></div>
+                <div class="extra mu-readmore-btn-disable" data-price="0"></div>
+                <div class="extra mu-readmore-btn-disable" data-price="0"></div>
+                <div class="extra mu-readmore-btn-disable" data-price="0"></div>
               </div>
               <div class="row" id=".amount" data-mustselect="false" data-type="hit">
                 <h3>จำนวน</h3>
                 <button id="plus" class="amount mu-readmore-btn" data-price="0">+</button>
                 <button id="minus" class="amount mu-readmore-btn" data-price="0">-</button>
                 <div id="amount" class="amount normal-btn bg-brown">1 ชาม</div>
+                <div id="price" class="price normal-btn bg-brown">1 บาท</div>
+                <div id="sum" class="sum normal-btn bg-brown">1 บาท</div>
+                <div id="amount" class="amount normal-btn bg-brown"></div>
               </div>
               <div class="row" id=".action" data-mustselect="false" data-type="none">
                 <h3>รายการอาหารที่สั่ง</h3>
-                <h4 id="lb-result"></h4>
-              </div>
-              <div class="row" id=".price" data-mustselect="false" data-type="none">
-                <h3>ราคาต่อชาม</h3>
-                <div id="price" class="price normal-btn bg-brown">1 บาท</div>
-              </div>
-              <div class="row" id=".sum" data-mustselect="false" data-type="none">
-                <h3>ราคาทั้งสิ้น</h3>
-                <div id="sum" class="sum normal-btn bg-brown">1 บาท</div>
-              </div>
-              <div class="row" id=".action" data-mustselect="false" data-type="none">
-                <h3>ส่งรายการ</h3>
+                <div id="lb-result" class="sum normal-list-name-btn bg-brown"></div>
                 <button id="order" class="action normal-btn bg-green">ORDER</button>
                 <button id="cancel" class="action normal-btn bg-red">CANCEL</button>
                 <input type="hidden" id="_token" name="_token" value="{{ csrf_token() }}">
